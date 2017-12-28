7558e32b6d10e9478b58d3c73828205c383d5b64
Kyle Jessup <kyle.jessup@treefrog.ca>
2017-12-28 16:12:42 -0500

    linking things

--- Package.swift
+++ Package.swift
@@ -1,25 +1,10 @@
 // swift-tools-version:4.0
 import PackageDescription
 let package = Package(
-    name: "PerfectCBSON",
+	name: "PerfectCBSON",
 	pkgConfig: "libbson-1.0",
 	providers: [
-		.brewItem(["mongo-c-driver"]),
-		.aptItem(["libbson-dev"]),
-	],
-    products: [
-        .library(
-            name: "PerfectCBSON",
-            targets: ["PerfectCBSON"]
-		),
-    ],
-    dependencies: [
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "PerfectCBSON",
-            dependencies: []
-		),
-    ]
+		.brew(["mongo-c-driver"]),
+		.apt(["libbson-dev"]),
+	]
 )
